7586fee75801b9b6143aad28907eeddad8231617
Anton Tcholako <anton.pt@outlook.com>
2016-05-02 14:00:04 +0100

    Add Endorphin.Core.NationalInstruments to solution

---
+++ src/Endorphin.Core.NationalInstruments/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+// Copyright (c) University of Warwick. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
+
+namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Endorphin.Core.NationalInstruments")>]
+[<assembly: AssemblyProductAttribute("Endorphin.Core")>]
+[<assembly: AssemblyDescriptionAttribute("Core infrastructure for scientific instrument control software development in F#.")>]
+[<assembly: AssemblyVersionAttribute("0.9.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.9.0"
+    let [<Literal>] InformationalVersion = "0.9.0"
